0c0fb50948e50033d189d4728e3bea1f8de3c111
victd <55512729+victd@users.noreply.github.com>
2022-03-22 13:19:40 -0400

    Update StripOBW.py

--- StripOBW.py
+++ StripOBW.py
@@ -15,7 +15,7 @@ doc = docx.Document()
 # CHANGE THIS TEXT FILE FOR THE DIVISION ----------------------------
 # Refer to the TRUX EDI extract template for the fields in csv format
 # for future revisions, see
-# third party company disclosure not needed as 
+# third party company disclosure not needed as appears to be standard trux template
 
 f = open("WT.txt", "r")
 f2 = open("WT-stripped.txt", "w")
