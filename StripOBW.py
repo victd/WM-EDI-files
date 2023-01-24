c096b3a03316dbadf58148d3e3e034e8cbc85cc5
victd <55512729+victd@users.noreply.github.com>
2023-01-24 08:53:32 -0500

    Update StripOBW.py

--- StripOBW.py
+++ StripOBW.py
@@ -24,7 +24,7 @@ doc = docx.Document()
 # we get feedback from Oakleaf, it will be 2-3 months behind as they process the data on their end
 # some consolidated billing stores may have trouble distinguishing sites under the same storeID
 # active directory connect sync issue
-# divide and conquer to resolve cumulation of issues
+# divide and conquer to resolve cumulation of issues, folder mappings
 
 f = open("WT.txt", "r")
 f2 = open("WT-stripped.txt", "w")
