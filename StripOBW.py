d550ba4f04384e10cbf215bd0c0eff87569737ae
victd <55512729+victd@users.noreply.github.com>
2023-08-14 10:10:24 -0400

    Update StripOBW.py

--- StripOBW.py
+++ StripOBW.py
@@ -23,6 +23,7 @@ doc = docx.Document()
 # will need to join tables at the service level first
 # we get feedback from Oakleaf, it will be 2-3 months behind as they process the data on their end
 # some consolidated billing stores may have trouble distinguishing sites under the same storeID
+# prefer to be under separate site billing
 # active directory connect sync issue azure
 # divide and conquer to resolve cumulation of issues, folder mappings
 
