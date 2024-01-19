e8c5a44c63f2dc62d53f89b10696c8822b9f1ae0
victd <55512729+victd@users.noreply.github.com>
2024-01-19 08:50:56 -0500

    Update StripOBW.py

--- StripOBW.py
+++ StripOBW.py
@@ -26,6 +26,7 @@ doc = docx.Document()
 # prefer to be under separate site billing
 # active directory connect sync issue azure, kayak, stand up paddle
 # divide and conquer to resolve cumulation of issues, folder mappings
+# elevator and hvac service
 
 f = open("WT.txt", "r")
 f2 = open("WT-stripped.txt", "w")
